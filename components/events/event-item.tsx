15d5ab563586b8ab2c4155bc8754c5dc0d00796a
yeonns <ysh099@gmail.com>
2021-11-20 13:52:22 +0900

    11/19

---
+++ components/events/event-item.tsx
@@ -0,0 +1,37 @@
+import Link from 'next/link'
+import Button from '../ui/button'
+import classes from './event-item.module.css'
+
+function EventItem(props) {
+  const { title, image, date, location, id } = props
+
+  const humanReadableDate = new Date(date).toLocaleDateString('en-US', {
+    day: 'numeric',
+    month: 'long',
+    year: 'numeric'
+  })
+  const formattedAddress = location.replace(', ', '\n')
+  const exploreLink = `/events/${id}`
+
+  return (
+    <li className={classes.item}>
+      <img src={'/' + image} alt={title} />
+      <div className={classes.content}>
+        <div className={classes.title}>
+          <h2>{title}</h2>
+          <div className={classes.date}>
+            <time>{humanReadableDate}</time>
+          </div>
+          <div className={classes.address}>
+            <address>{formattedAddress}</address>
+          </div>
+        </div>
+        <div className={classes.actions}>
+          <Button link={exploreLink}>Explore Event</Button>
+        </div>
+      </div>
+    </li>
+  )
+}
+
+export default EventItem

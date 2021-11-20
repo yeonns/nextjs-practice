dd97904b94a15f1a1480800a380461c1e8b2c975
yeonns <ysh099@gmail.com>
2021-11-20 14:54:23 +0900

    add filter exception

--- pages/events/[...slug].tsx
+++ pages/events/[...slug].tsx
@@ -1,7 +1,43 @@
+import { useRouter } from "next/router"
+import { getFilteredEvents } from "../../dummy-data"
+
 function FilteredEventsPage() {
+  const router = useRouter()
+  const filterData = router.query.slug
+
+  if (!filterData) {
+    return <p className='center'>Loading...</p>
+  }
+
+  const filteredYear = filterData[0]
+  const filteredMonth = filterData[1]
+
+  const numYear = +filteredYear
+  const numMonth = +filteredMonth
+
+  if (
+    isNaN(numYear) || 
+    isNaN(numMonth) || 
+    numYear > 2030 || 
+    numYear < 2021 || 
+    numMonth < 1 || 
+    numMonth > 12
+  ) {
+    return <p>Invalid Filter. Please adjust your values!</p>
+  }
+
+  const filteredEvents = getFilteredEvents({
+    year: numYear,
+    month: numMonth, 
+  })
+
+  if (!filteredEvents || filteredEvents.length === 0) {
+    return <p>No events found for the chosen filter!</p>
+  } 
+
   return (
     <div>
-      
+
     </div>
   )
 }

a032bceefcbae91e696bd32294c80499071806c6
yeonns <ysh099@gmail.com>
2021-11-20 14:24:04 +0900

    add all events page

---
+++ pages/events/index.tsx
@@ -0,0 +1,14 @@
+import EventList from "../../components/events/event-list"
+import { getAllEvents } from "../../dummy-data"
+
+function AllEventsPage() {
+  const events = getAllEvents()
+
+  return (
+    <div>
+      <EventList items={events} />
+    </div>
+  )
+}
+
+export default AllEventsPage

fd513e96c0f6bdc740af122de06876e5ca5b78f4
yeonns <ysh099@gmail.com>
2021-11-20 14:36:31 +0900

    add event search

---
+++ pages/events/[...slug].tsx
@@ -0,0 +1,9 @@
+function FilteredEventsPage() {
+  return (
+    <div>
+
+    </div>
+  )
+}
+
+export default FilteredEventsPage
--- pages/events/index.tsx
+++ pages/events/index.tsx
@@ -1,13 +1,16 @@
+import { Fragment } from "react"
 import EventList from "../../components/events/event-list"
+import EventsSearch from "../../components/events/events-search"
 import { getAllEvents } from "../../dummy-data"
 
 function AllEventsPage() {
   const events = getAllEvents()
 
   return (
-    <div>
+    <Fragment>
+      <EventsSearch />
       <EventList items={events} />
-    </div>
+    </Fragment>
   )
 }
 

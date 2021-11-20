416f39cd59c64fb2c6a4184608dadd6a068b3ed8
yeonns <ysh099@gmail.com>
2021-11-20 13:54:15 +0900

    modify classname

--- components/events/event-item.tsx
+++ components/events/event-item.tsx
@@ -17,7 +17,7 @@ function EventItem(props) {
     <li className={classes.item}>
       <img src={'/' + image} alt={title} />
       <div className={classes.content}>
-        <div className={classes.title}>
+        <div className={classes.summary}>
           <h2>{title}</h2>
           <div className={classes.date}>
             <time>{humanReadableDate}</time>

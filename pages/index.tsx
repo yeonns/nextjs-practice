e4cdd3d4cff2036ee9b7fd6a7f7a35e17b202952
yeonns <ysh099@gmail.com>
2021-11-21 14:09:35 +0900

    fetch data from server

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,6 @@
+import path from 'path'
+import fs from 'fs/promises'
+
 function HomePage(props) {
   const { products } = props
 
@@ -11,14 +14,13 @@ function HomePage(props) {
 }
 
 export async function getStaticProps() {
+  const filePath = path.join(process.cwd(), 'data', 'dummy-backend.json')
+  const jsonData = await fs.readFile(filePath)
+  const data = JSON.parse(jsonData)
+
   return {
     props: {
-      products: [
-        {
-          id: 'p1',
-          title: 'Product 1'
-        }
-      ]
+      products: data.products
     }
   }
 }

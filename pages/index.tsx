aadca4e32169b44b16b404803101ec6156ba6d52
yeonns <ysh099@gmail.com>
2021-11-23 22:11:25 +0900

    ISR

--- pages/index.tsx
+++ pages/index.tsx
@@ -14,6 +14,7 @@ function HomePage(props) {
 }
 
 export async function getStaticProps() {
+  console.log('(Re-)Generating...')
   const filePath = path.join(process.cwd(), 'data', 'dummy-backend.json')
   const jsonData = await fs.readFile(filePath)
   const data = JSON.parse(jsonData)
@@ -21,7 +22,8 @@ export async function getStaticProps() {
   return {
     props: {
       products: data.products
-    }
+    },
+    revalidate: 120  // seconds
   }
 }
 

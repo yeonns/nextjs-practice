ace2e9649a7992fae8046b0381553f06ed8a20e6
yeonns <ysh099@gmail.com>
2021-11-23 22:36:44 +0900

    add getstaticpaths

--- pages/[pid].tsx
+++ pages/[pid].tsx
@@ -30,4 +30,15 @@ export async function getStaticProps(context) {
   }
 }
 
+export async function getStaticPaths() {
+  return {
+    paths: [
+      { params: { pid: 'p1' } },
+      { params: { pid: 'p2' } },
+      { params: { pid: 'p3' } },
+    ],
+    fallback: false,
+  }
+}
+
 export default ProductDetailPage

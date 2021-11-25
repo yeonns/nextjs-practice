669e52f038e1ebf8bdd9e5d6c84a51ec1a9d459f
yeonns <ysh099@gmail.com>
2021-11-25 23:10:27 +0900

    useSWR

--- pages/last-sales.tsx
+++ pages/last-sales.tsx
@@ -1,9 +1,29 @@
 import { useEffect, useState } from "react"
+import useSWR from 'swr'
 
-function LastSalesPage() {
-  const [sales, setSales] = useState()
-  const [isLoading, setIsLoading] = useState(false)
+function LastSalesPage(props) {
+  const [sales, setSales] = useState(props.sales)
+//  const [isLoading, setIsLoading] = useState(false) 
 
+  const { data, error } = useSWR(
+    'https://react-course-7ca7c-default-rtdb.firebaseio.com/sales.json'
+  )
+
+  useEffect(() => {
+    if (data) {
+      const transformedSales = []
+
+      for(const key in data) {
+        transformedSales.push({
+          id: key, 
+          username: data[key].username, 
+          volume: data[key].volume
+        })
+      }
+      setSales(transformedSales)
+    }
+  }, [data])
+/*
   useEffect(() => {
     setIsLoading(true)
     fetch(
@@ -19,24 +39,45 @@ function LastSalesPage() {
       setSales(transformedSales)
       setIsLoading(false)
     })
-  }, [])
-
-  if (isLoading) {
-    return <p>Loading...</p>
+  }, []) */
+  if (error) {
+    return <p>Failed to load.</p>
   }
 
-  if (!sales) {
-    return <p>No data yet</p>
+  if (!data && !sales) {
+    return <p>Loading...</p>
   }
 
   return (
     <ul>
-      {sales.map(sale => 
+      {sales.map((sale) => (
         <li key={sale.id}>
-          {sale.username} - {sale.volume}
-        </li>)}
+          {sale.username} - ${sale.volume}
+        </li>
+      ))}
     </ul>
   )
 }
+
+export async function getStaticProps() {
+  return fetch('https://react-course-7ca7c-default-rtdb.firebaseio.com/sales.json')
+    .then(response => response.json())
+    .then(data => {
+      const transformedSales = []
+
+      for(const key in data) {
+        transformedSales.push({
+          id: key, 
+          username: data[key].username, 
+          volume: data[key].volume
+        })
+      }
+
+      return {
+        props: { sales: transformedSales },
+        revalidate: 10,
+      }
+    })
+}  
 
 export default LastSalesPage

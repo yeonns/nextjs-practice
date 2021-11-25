3f33cc6801c557410cc0a5fb2d6cdafcb7da4443
yeonns <ysh099@gmail.com>
2021-11-25 21:54:24 +0900

    loading path dynamically

--- pages/[pid].tsx
+++ pages/[pid].tsx
@@ -21,9 +21,7 @@ export async function getStaticProps(context) {
   const { params } = context
 
   const productId = params.pid
-  const filePath = path.join(process.cwd(), 'data', 'dummy-backend.json')
-  const jsonData = await fs.readFile(filePath)
-  const data = JSON.parse(jsonData)
+  const data = await getData()
 
   const product = data.products.find(product => product.id === productId)
 
@@ -34,12 +32,22 @@ export async function getStaticProps(context) {
   }
 }
 
+async function getData() {
+  const filePath = path.join(process.cwd(), 'data', 'dummy-backend.json')
+  const jsonData = await fs.readFile(filePath)
+  const data = JSON.parse(jsonData) 
+
+  return data
+}
+
 export async function getStaticPaths() {
+  const data = await getData()
+  const ids = data.products.map(product => product.id)
+  const pathsWithParams = ids.map(id => ({ params: { pid: id }}))
+
   return {
-    paths: [
-      { params: { pid: 'p1' } },
-    ],
-    fallback: true,
+    paths: pathsWithParams,
+    fallback: false,
     //fallback: 'blocking', data fetch가 끝나기전에는 어떤 page도 띄워주지않음 if(!loadedProduct) { return <p>Loading...</p> } 제거 가능
   }
 }

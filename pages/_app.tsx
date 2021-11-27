1e3c414d6daadeb4b2c3722f1f9c6968e0a8b65d
yeonns <ysh099@gmail.com>
2021-11-27 22:32:46 +0900

    define Head at _app

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,3 +1,4 @@
+import Head from 'next/head'
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import Layout from '../components/layout/layout'
@@ -5,6 +6,9 @@ import Layout from '../components/layout/layout'
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <Layout>
+      <Head>
+        <meta name='viewport' content='initial-scale=1.0, width=device-width' />
+      </Head>
       <Component {...pageProps} />
     </Layout>
   )

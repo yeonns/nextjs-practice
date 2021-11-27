c62667a0a1f3ddf203fcf15ec821e6a43b52b7ec
yeonns <ysh099@gmail.com>
2021-11-27 21:32:50 +0900

    dynamic pages

--- pages/events/[eventId].tsx
+++ pages/events/[eventId].tsx
@@ -1,16 +1,12 @@
-import { useRouter } from "next/router"
 import { Fragment } from "react"
 import EventContent from "../../components/event-detail/event-content"
 import EventLogistics from "../../components/event-detail/event-logistics"
 import EventSummary from "../../components/event-detail/event-summary"
 import ErrorAlert from "../../components/ui/error-alert"
-import { getEventById } from "../../dummy-data"
+import { getAllEvents, getEventById } from "../../helpers/api-util"
 
-function EventDetailPage() {
-  const router = useRouter()
-
-  const eventId = router.query.eventId
-  const event = getEventById(eventId)
+function EventDetailPage(props) {
+  const event = props.selectedEvent
 
   if (!event) {
     return (
@@ -29,6 +25,29 @@ function EventDetailPage() {
       </EventContent>
     </Fragment>
   )
+}
+
+export async function getStaticProps(context) {
+  const eventId = context.params.eventId
+  const event = await getEventById(eventId)
+
+  return {
+    props: {
+      selectedEvent: event
+    }
+  }
+}
+
+export async function getStaticPaths() {
+  const events = await getAllEvents()
+  const paths = events.map((event) => ({
+    params: { eventId: event.id }
+  }))
+
+  return {
+    paths: paths,
+    fallback: false,
+  }
 }
 
 export default EventDetailPage

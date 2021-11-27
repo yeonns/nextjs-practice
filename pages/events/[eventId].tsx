58b0df386d3f73382d8248b7a2787003075769d3
yeonns <ysh099@gmail.com>
2021-11-27 21:40:48 +0900

    optimize data fetching

--- pages/events/[eventId].tsx
+++ pages/events/[eventId].tsx
@@ -3,16 +3,16 @@ import EventContent from "../../components/event-detail/event-content"
 import EventLogistics from "../../components/event-detail/event-logistics"
 import EventSummary from "../../components/event-detail/event-summary"
 import ErrorAlert from "../../components/ui/error-alert"
-import { getAllEvents, getEventById } from "../../helpers/api-util"
+import { getEventById, getFeaturedEvents } from "../../helpers/api-util"
 
 function EventDetailPage(props) {
   const event = props.selectedEvent
 
   if (!event) {
     return (
-      <ErrorAlert>
-        <p>No event found!</p>
-      </ErrorAlert>
+      <div className='center'>
+        <p>Loading...</p>
+      </div>
     )
   }
 
@@ -34,19 +34,20 @@ export async function getStaticProps(context) {
   return {
     props: {
       selectedEvent: event
-    }
+    },
+    revalidate: 30,
   }
 }
 
 export async function getStaticPaths() {
-  const events = await getAllEvents()
+  const events = await getFeaturedEvents()
   const paths = events.map((event) => ({
     params: { eventId: event.id }
   }))
 
   return {
     paths: paths,
-    fallback: false,
+    fallback: 'blocking',
   }
 }
 
